3b485a6287bd4723603d288cf42250082fb9a112
keg66 <keg@keg66.com>
2022-06-18 15:50:09 +0900

    add stop()

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,89 @@
-use std::sync::Arc;
+use std::{
+    sync::{atomic::AtomicBool, atomic::Ordering, Arc},
+    thread::JoinHandle,
+};
 
 struct Timer {
     duration: f64,
     callback: Arc<fn() -> ()>,
+    handler: Option<JoinHandle<()>>,
+    is_running: Arc<AtomicBool>,
 }
 
 impl Timer {
-    fn new(duration: f64, callback: Arc<fn() -> ()>) -> Timer {
-        Timer { duration, callback }
+    fn new(duration: f64, callback: fn() -> ()) -> Timer {
+        Timer {
+            duration,
+            callback: Arc::new(callback),
+            handler: None,
+            is_running: Arc::new(AtomicBool::new(false)),
+        }
     }
 
-    fn start(&self) {
+    fn start(&mut self) {
         let dur = self.duration;
         let local_callback = self.callback.clone();
-        let handler = std::thread::spawn(move || loop {
-            let now = std::time::Instant::now();
 
-            local_callback();
+        let local_is_running = self.is_running.clone();
+        local_is_running.store(true, Ordering::SeqCst);
 
-            let process_time = now.elapsed().as_secs_f64();
+        self.handler = Some(std::thread::spawn(move || {
+            while local_is_running.load(Ordering::SeqCst) {
+                let now = std::time::Instant::now();
 
-            if dur > process_time {
-                let duration = std::time::Duration::from_secs_f64(dur - process_time);
-                spin_sleep::sleep(duration);
+                local_callback();
+
+                let process_time = now.elapsed().as_secs_f64();
+
+                if dur > process_time {
+                    let duration = std::time::Duration::from_secs_f64(dur - process_time);
+                    spin_sleep::sleep(duration);
+                }
             }
-        });
+        }));
+    }
 
-        handler.join().unwrap();
+    fn stop(&mut self) {
+        self.is_running.store(false, Ordering::SeqCst);
+
+        if let Some(handler) = self.handler.take() {
+            handler.join().unwrap();
+        }
     }
 }
 
-fn f() {
-    println!("{:?}", std::time::Instant::now());
+impl Drop for Timer {
+    fn drop(&mut self) {
+        self.stop();
+    }
+}
+
+fn timer_callback() {
+    println!("[timer_callback] {:?}", std::time::Instant::now());
 }
 
 fn main() {
-    let timer = Timer::new(1.0, Arc::new(f));
+    let mut timer = Timer::new(0.5, timer_callback);
     timer.start();
+
+    let mut timer2 = Timer::new(0.1, || {
+        println!("[timer_callback closure] {:?}", std::time::Instant::now());
+    });
+
+    let dur = std::time::Duration::from_secs(1);
+    let mut count = 0;
+    loop {
+        println!("[main] {:?}", std::time::Instant::now());
+        spin_sleep::sleep(dur);
+
+        count += 1;
+        if count == 5 {
+            println!("stop timer");
+            timer.stop();
+        }
+        if count == 10 {
+            println!("start timer2");
+            timer2.start();
+        }
+    }
 }
