c6c7b8de0a3235d50c89495d338b173646d1965a
keg66 <keg@keg66.com>
2022-06-18 16:27:54 +0900

    add capture_sample.rs

---
+++ bin/capture_sample.rs
@@ -0,0 +1,17 @@
+use simple_timer::Timer;
+use std::sync::atomic::{AtomicU64, Ordering};
+
+fn main() {
+    let time = std::time::Instant::now();
+    let prev = AtomicU64::new(time.elapsed().as_micros() as u64);
+    let mut timer = Timer::new(0.1, move || {
+        let time = time.elapsed().as_micros() as u64;
+
+        println!("interval: {} us", time - prev.load(Ordering::SeqCst));
+        prev.store(time, Ordering::SeqCst);
+    });
+
+    timer.start();
+
+    loop {}
+}
